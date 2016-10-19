47435cc9e8b261ec9752fad22c3ab66d24e74c03
Roman Random <roman@romanrandom.com>
2016-10-19 18:54:44 +0200

    start

---
+++ src/client/player/PlayerRenderer.js
@@ -0,0 +1,84 @@
+import { Renderer } from 'soundworks/client';
+import * as konva from 'konva';
+
+/**
+ * A simple canvas renderer.
+ * The class renders a dot moving over the screen and rebouncing on the edges.
+ */
+export default class PlayerRenderer extends Renderer {
+  constructor(tapCallback, numImages) {
+    super(0); // update rate = 0: synchronize updates to frame rate
+
+    this.tapCallback = tapCallback;
+
+    this.stage = new Konva.Stage({
+        container: 'experience',
+        width: window.innerWidth,
+        height: window.innerHeight
+    });
+
+    this.stickers = new Array(numImages);
+  }
+
+  /**
+   * Initialize renderer state.
+   */
+   init() {
+
+    this.layer = new Konva.Layer();
+
+    for (var i = 0; i < this.stickers.length; i++) {
+        this.stickers[i] = new Image();
+
+        this.stickers[i].onload = (e) => {
+
+            const antimg = new Konva.Image({
+                x: Math.random() * this.stage.attrs.width,
+                y: Math.random() * this.stage.attrs.height,
+                image: e.path[0],
+                width: 150,
+                height: 150,
+                rotation: Math.random() * 60,
+                draggable: true
+            });
+
+            //antimg.offsetX(antimg.width / 2);
+            //antimg.offsetY(antimg.height / 2);
+
+            antimg.on('tap', () => {
+                this.tapCallback(i);
+            });
+
+            // add the shape to the layer
+            this.layer.add(antimg);
+
+            // add the layer to the stage
+            this.stage.add(this.layer);
+        };
+        this.stickers[i].src = `stickers/stick${i + 1}.jpg`;
+    }
+    console.log(this.layer);
+
+
+}
+
+  /**
+   * Update rederer state.
+   * @param {Number} dt - time since last update in seconds.
+   */
+  update(dt) {
+
+  }
+
+  /**
+   * Draw into canvas.
+   * Method is called by animation frame loop in current frame rate.
+   * @param {CanvasRenderingContext2D} ctx - canvas 2D rendering context
+   */
+  render(ctx) {
+    // canvas operations
+    ctx.save();
+
+    ctx.restore();
+  }
+}
---
+++ src/server/AntiExperience.js
@@ -0,0 +1,44 @@
+import { Experience } from 'soundworks/server';
+
+// server-side 'player' experience.
+export default class PlayerExperience extends Experience {
+
+  constructor(clientTypes) {
+    super(clientTypes);
+
+    this.checkin = this.require('checkin');
+    this.osc = this.require('osc');
+    this.sharedConfig = this.require('shared-config');
+
+    this.lastTeam = 0; // Team assigned to the last player who joined
+  }
+
+  // if anything needs to append when the experience starts
+  start() {
+  }
+
+  // if anything needs to happen when a client enters the performance (*i.e.*
+  // starts the experience on the client side), write it in the `enter` method
+  enter(client) {
+
+    super.enter(client);
+    // send a message to all the other clients of the same type
+    //this.broadcast(client.type, client, 'play');
+
+    // Asignamos equipo alternadamente al nuevo jugador
+    if (this.lastTeam === 0) {
+        client.team = this.lastTeam = 1;
+    } else {
+        client.team = this.lastTeam = 0;
+    }
+
+    this.receive(client, 'sticker', sticker => {
+        this.osc.send('/osc/channel2', [client.index, client.team, sticker]);
+    });
+  }
+
+  exit(client) {
+    super.exit(client);
+    // ...
+  }
+}
